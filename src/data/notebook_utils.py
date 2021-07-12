90866393a1077a59e4194c933cb88d379f205176
pacasi <mticona@dc.uba.ar>
2021-07-12 11:08:17 -0300

    [Refactored] Upload from branch Pendleton replication

--- src/data/notebook_utils.py
+++ src/data/notebook_utils.py
@@ -22,35 +22,28 @@ def read_data(data_path=training_dataset_path):
 
 
 def read_chemical_info():
+    """Read information about chemical compounds used as organoammonium."""
     with open(chemical_inventory_path) as file:
         chemical_inventory = pd.read_csv(file, header=0)
         return chemical_inventory
 
 
 def filter_columns_by_prefix(columns, prefixes):
-    filtered_columns = { column for column in columns if True in (column.startswith(prefix)  for prefix in prefixes) }
+    """Filter columns by prefix."""
+    filtered_columns = {column for column in columns 
+                        if True in (column.startswith(prefix) 
+                        for prefix in prefixes)}
     return filtered_columns
 
 
 def get_columns(total_columns):
-    prefixs = ['_rxn_', '_feat_', '_raw']
+    """Get all used columns: Reaction condition and physicochemical features."""
+    prefixs = ['_rxn_', '_feat_']
     columns_by_prefix = {}
     for prefix in prefixs:
-        columns_by_prefix[prefix] = set(filter(lambda column_name: column_name.startswith(prefix), total_columns))
+        columns_by_prefix[prefix] = set(filter(lambda column_name: 
+                                        column_name.startswith(prefix), 
+                                        total_columns))
     columns_by_prefix['solUD'] = set(data_utils.get_sol_ud_model_columns(total_columns))
-    columns_by_prefix['solV'] = set(data_utils.get_sol_v_model_columns(total_columns))
-    chem_cols = []
-    data_utils.extend_with_chem_columns(total_columns, chem_cols)
-    columns_by_prefix['chem'] = set(chem_cols)
     return columns_by_prefix
 
-
-# We limit the scope only analizing solUD information, we could have used solV
-def get_used_columns(total_columns):
-    to_add = []
-    data_utils.extend_with_rxn_columns(total_columns, to_add, sol_ud_enable=True)
-    #data_utils.extend_with_rxn_columns(total_columns, to_add, sol_ud_enable=False)
-    data_utils.extend_with_chem_columns(total_columns, to_add)
-
-    #to_add += '_raw_v0-M_acid _raw_v0-M_inorganic _raw_v0-M_organic'.split(' ')
-    return set(to_add)
